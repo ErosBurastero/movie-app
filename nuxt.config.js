1502944843b84ac53bb95fcc3a873c51b39eda03
Eros <erosburastero_it@hotmail.com>
2023-06-27 21:42:10 -0300

    creating dot env file and setting up styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,9 @@ export default {
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [],
+    css: [
+        '~/assets/scss/main.scss',
+    ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: ['~/plugins/services.js', ],
@@ -48,7 +50,7 @@ export default {
 
     // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
     vuetify: {
-        customVariables: ['~/assets/variables.scss'],
+        customVariables: ['~/assets/scss/variables.scss'],
         theme: {
             dark: true,
             themes: {
