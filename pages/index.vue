b46929c7c3746e220d03fa71bf85548899b1fec8
Eros <erosburastero_it@hotmail.com>
2023-06-27 22:20:09 -0300

    providing parameters to http request

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>HOLA</div>
+  <div>
+    <v-container>
+      {{ movies }}
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
   data() {
-    return {}
+    return {
+      movies: [],
+    }
+  },
+  async created() {
+    try {
+      const response = await this.$getMovies()
+      this.movies = response
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
   },
 }
 </script>
