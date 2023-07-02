d0a62a6709afe2e0e94b3a15b6f84b3ce790b6b2
Eros <erosburastero_it@hotmail.com>
2023-07-02 13:49:08 -0300

    creating a reusable card for movies

---
+++ components/cards/MovieCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row class="d-flex justify-center">
+    <v-col
+      v-for="(movie, index) in movies"
+      :key="index"
+      cols="6"
+      lg="3"
+      md="4"
+      xl="2"
+      class="align-stretch w-100"
+    >
+      <Card
+        v-bind="$attrs"
+        cardClass="h-100 pb-4"
+        nuxt
+        :to="{
+          name: 'peliculas-id',
+          params: { id: movie.imdbID },
+        }"
+      >
+        <template #content>
+          <VuetifyImage
+            :src="
+              movie.Poster === 'N/A' ? '/images/noAvailable.jpg' : movie.Poster
+            "
+            imageClass="image"
+            :max-height="$vuetify.breakpoint.mdAndUp ? 330 : 200"
+          />
+          <v-card-title
+            :class="[
+              'white--text',
+              $vuetify.breakpoint.smAndDown && 'subtitle-1',
+            ]"
+            >{{ movie.Title }}</v-card-title
+          >
+          <v-card-subtitle class="white--text">{{
+            'Año ' + movie.Year
+          }}</v-card-subtitle>
+        </template>
+      </Card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    movies: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/peliculas/index.vue
+++ pages/peliculas/index.vue
@@ -1,49 +1,7 @@
 <template>
   <div class="bg-movies">
-    <v-container fluid class="pa-10" v-if="movies && response">
-      <v-row class="d-flex justify-center">
-        <v-col
-          v-for="(movie, index) in movies"
-          :key="index"
-          cols="6"
-          lg="3"
-          md="4"
-          xl="2"
-          class="align-stretch w-100"
-        >
-          <Card
-            cardClass="h-100 pb-4"
-            :color="blue"
-            nuxt
-            :to="{
-              name: 'peliculas-id',
-              params: { id: movie.imdbID },
-            }"
-          >
-            <template #content>
-              <VuetifyImage
-                :src="
-                  movie.Poster === 'N/A'
-                    ? '/images/noAvailable.jpg'
-                    : movie.Poster
-                "
-                imageClass="image"
-                :max-height="$vuetify.breakpoint.mdAndUp ? 330 : 200"
-              />
-              <v-card-title
-                :class="[
-                  'white--text',
-                  $vuetify.breakpoint.smAndDown && 'subtitle-1',
-                ]"
-                >{{ movie.Title }}</v-card-title
-              >
-              <v-card-subtitle class="white--text">{{
-                'Año ' + movie.Year
-              }}</v-card-subtitle>
-            </template>
-          </Card>
-        </v-col>
-      </v-row>
+    <v-container fluid class="pa-10" v-if="movies">
+      <MovieCard :movies="movies" :color="blue" />
     </v-container>
     <div v-else class="h-100vh d-flex justify-center pa-10">
       <VuetifyImage
